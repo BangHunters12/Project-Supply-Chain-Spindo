4784716319571b5df03489a5b92f53e0941e885c
Dendi Aprilio <gelasajur1209@gmail.com>
2026-08-27 21:43:07 +0700

    feat: schedule sikuta sync to run automatically every minute

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Jalankan sinkronisasi otomatis setiap menit
+Schedule::command('sikuta:sync --force')->everyMinute();
+
